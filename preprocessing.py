d9baf27a7783e34356e9c2a0abf54211d8adcbcc
Heeyong <heeyonghuh@gmail.com>
2022-06-28 18:45:13 -0500

    Filter object is completed

--- preprocessing.py
+++ preprocessing.py
@@ -1,25 +1,27 @@
 import os
-from sqlite3 import Row
-from xmlrpc.client import _Method
-import numpy as np
+import random
 import matplotlib
 import matplotlib.pyplot as plt
-
 import mne
 matplotlib.use('TkAgg')
 
 class Filtering:
-    def __init__(self, raw):
+    def __init__(self, raw, l_freq, h_freq, nf_freqs=(60,120,180)):
         # filter parameters
-        self.raw = raw
+        self.raw = raw.copy()
         self.sfreq = raw.info['sfreq']
         self.picks = ['eeg', 'eog']
+        self.l_freq = l_freq
+        self.h_freq = h_freq
+        self.nf_freqs = nf_freqs
 
-    def highpass(self, cutoff_freq, filt_type='fir', iir_params=None, show_filt_params = False):
+    def highpass(self, cutoff_freq=None, filt_type='fir', iir_params=None, show_filt_params = False, verbose='warning'):
+        if cutoff_freq == None:
+            cutoff_freq = self.l_freq
         filt_raw = self.raw.filter(
             l_freq=cutoff_freq, h_freq=None,
             picks=self.picks, method=filt_type,
-            iir_params=iir_params)
+            iir_params=iir_params, verbose=verbose)
         if show_filt_params == True:
             # check the filter parameter
             filter_params = mne.filter.create_filter(
@@ -30,11 +32,13 @@ class Filtering:
                 sfreq=self.sfreq, flim=(0.01, self.sfreq/2))
         return filt_raw
 
-    def lowpass(self, cutoff_freq, filt_type='fir', iir_params=None, show_filt_params = False):
+    def lowpass(self, cutoff_freq=None, filt_type='fir', iir_params=None, show_filt_params = False, verbose='warning'):
+        if cutoff_freq == None:
+            cutoff_freq = self.h_freq
         filt_raw = self.raw.filter(
             l_freq=None, h_freq=cutoff_freq,
             picks=self.picks, method=filt_type,
-            iir_params=iir_params)
+            iir_params=iir_params, verbose=verbose)
         if show_filt_params == True:
             # check the filter parameter
             filter_params = mne.filter.create_filter(
@@ -45,14 +49,41 @@ class Filtering:
                 sfreq=self.sfreq, flim=(0.01, self.sfreq/2))
         return filt_raw
 
-    def notch(self, target_freqs=60, notch_wdith=None, filt_type='fir', iir_params=None):
+    def notch(self, target_freqs=None, notch_wdith=None, filt_type='fir', iir_params=None, verbose='warning'):
+        if target_freqs == None:
+            target_freqs = self.nf_freqs
         filt_raw = self.raw.notch_filter(
             freqs=target_freqs, picks=self.picks,
-            method=filt_type, iir_params=iir_params)
+            method=filt_type, iir_params=iir_params, verbose=verbose)
         return filt_raw
 
-    def resample(self):
-        pass
+    def resample(self,new_sfreq=None, window='boxcar', events=None, verbose='warning'):
+        if new_sfreq == None:
+            new_sfreq == 4*self.h_freq
+        if new_sfreq > self.sfreq:
+            print('Error: New sampling frequency is greather than previous sampling freqeuncy. (Recommended new_f_s = 4 x high_f_c)')
+            return
+        filt_raw = self.raw.resample(sfreq=new_sfreq,
+            napd='auto', window=window, events=events, verbose=verbose)
+        return filt_raw
+
+    def external_artifact_rejection(self):
+        filt_raw = self.highpass()
+        filt_raw = self.notch()
+        filt_raw = self.lowpass()
+        filt_raw = self.resample()
+        return filt_raw
+    
+class Indepndent_Component_Analysis:
+    def __init__(self,raw, n_components=None,seed=random.randint(1,100), max_iter= 'auto'):
+        if n_components == None:
+            n_components = raw.info['nchan']
+        self.ica = mne.preprocessing.ICA(n_components=n_components, random_state=seed,max_iter= max_iter)
+        self.ica.fit(raw) #whitening and n-comps ICA performed
+    
+    def visuallize_ICA_components(self):
+        self.ica.plot_components
+        plt.show()
 
     
     pass
