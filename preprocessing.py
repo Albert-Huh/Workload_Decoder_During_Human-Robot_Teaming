51c3cc127e244cd9db73d2be2c39d7e26821ea6b
Heeyong <heeyonghuh@gmail.com>
2022-06-30 08:53:46 -0500

    Completed Preprocessing code

--- preprocessing.py
+++ preprocessing.py
@@ -15,41 +15,41 @@ class Filtering:
         self.h_freq = h_freq
         self.nf_freqs = nf_freqs
 
-    def highpass(self, cutoff_freq=None, filt_type='fir', iir_params=None, show_filt_params = False, verbose='warning'):
+    def highpass(self, cutoff_freq=None, filt_type='fir', iir_params=None,
+        show_filt_params = False, verbose='warning'):
+        
         if cutoff_freq == None:
             cutoff_freq = self.l_freq
         filt_raw = self.raw.filter(
-            l_freq=cutoff_freq, h_freq=None,
-            picks=self.picks, method=filt_type,
+            l_freq=cutoff_freq, h_freq=None, picks=self.picks, method=filt_type,
             iir_params=iir_params, verbose=verbose)
         if show_filt_params == True:
             # check the filter parameter
             filter_params = mne.filter.create_filter(
-                data=self.raw.get_data(), sfreq=self.sfreq,
-                l_freq=cutoff_freq, h_freq=None,
-                method=filt_type, iir_params=iir_params)
-            mne.viz.plot_filter(h=filter_params, 
-                sfreq=self.sfreq, flim=(0.01, self.sfreq/2))
+                data=self.raw.get_data(), sfreq=self.sfreq, l_freq=cutoff_freq, 
+                h_freq=None, method=filt_type, iir_params=iir_params)
+            mne.viz.plot_filter(h=filter_params, sfreq=self.sfreq, flim=(0.01, self.sfreq/2))
         return filt_raw
 
-    def lowpass(self, cutoff_freq=None, filt_type='fir', iir_params=None, show_filt_params = False, verbose='warning'):
+    def lowpass(self, cutoff_freq=None, filt_type='fir', iir_params=None, 
+        show_filt_params = False, verbose='warning'):
+        
         if cutoff_freq == None:
             cutoff_freq = self.h_freq
         filt_raw = self.raw.filter(
-            l_freq=None, h_freq=cutoff_freq,
-            picks=self.picks, method=filt_type,
+            l_freq=None, h_freq=cutoff_freq, picks=self.picks, method=filt_type,
             iir_params=iir_params, verbose=verbose)
         if show_filt_params == True:
             # check the filter parameter
             filter_params = mne.filter.create_filter(
-                data=self.raw.get_data(), sfreq=self.sfreq,
-                l_freq=cutoff_freq, h_freq=None,
-                method=filt_type, iir_params=iir_params)
-            mne.viz.plot_filter(h=filter_params, 
-                sfreq=self.sfreq, flim=(0.01, self.sfreq/2))
+                data=self.raw.get_data(), sfreq=self.sfreq, l_freq=cutoff_freq, 
+                h_freq=None, method=filt_type, iir_params=iir_params)
+            mne.viz.plot_filter(h=filter_params, sfreq=self.sfreq, flim=(0.01, self.sfreq/2))
         return filt_raw
 
-    def notch(self, target_freqs=None, notch_wdith=None, filt_type='fir', iir_params=None, verbose='warning'):
+    def notch(self, target_freqs=None, notch_wdith=None, filt_type='fir', 
+        iir_params=None, verbose='warning'):
+        
         if target_freqs == None:
             target_freqs = self.nf_freqs
         filt_raw = self.raw.notch_filter(
@@ -58,6 +58,7 @@ class Filtering:
         return filt_raw
 
     def resample(self,new_sfreq=None, window='boxcar', events=None, verbose='warning'):
+        
         if new_sfreq == None:
             new_sfreq == 4*self.h_freq
         if new_sfreq > self.sfreq:
@@ -68,6 +69,7 @@ class Filtering:
         return filt_raw
 
     def external_artifact_rejection(self):
+        
         filt_raw = self.highpass()
         filt_raw = self.notch()
         filt_raw = self.lowpass()
@@ -75,21 +77,26 @@ class Filtering:
         return filt_raw
     
 class Indepndent_Component_Analysis:
-    def __init__(self,raw, n_components=None,seed=random.randint(1,100), max_iter='auto', find_eog_peaks=True,find_ecg_peaks=False):
+    def __init__(self,raw, n_components=None,seed=random.randint(1,100), max_iter='auto', 
+        find_eog_peaks=True,find_ecg_peaks=False):
+        
         self.raw = raw
         self.n_components = raw.info['nchan']-1 if n_components==None else n_components
         self.random_state = seed
         self.max_iter = max_iter
         self.find_eog_peaks = find_eog_peaks
         self.find_ecg_peaks = find_ecg_peaks    
-        self.ica = mne.preprocessing.ICA(n_components=n_components, random_state = seed,max_iter= max_iter)
+        self.ica = mne.preprocessing.ICA(n_components=n_components, random_state=seed, 
+            max_iter= max_iter)
         self.eog_evoked = None
         self.ecg_evoked = None
 
     def setup_ICA(self):
+
         self.ica.fit(self.raw) # whitening and n-comps ICA performed
         
     def visualize_ICA_components(self):
+        
         self.ica.plot_components
         plt.show()
         self.ica.plot_sources(self.raw)
@@ -99,26 +106,35 @@ class Indepndent_Component_Analysis:
         ica_picks = list(np.arange(0,self.n_components-1,1))
         self.plot_properties(self.raw, picks=ica_picks)
 
-    def exclude_ica(self, list_exclude):
+    def exclude_ica(self, list_exclude=[]):
+
         self.ica.exclude = list_exclude
     
     def create_physsiological_evoked(self, baseline=(-0.5, -0.2), verbose='warning'):
+
         if self.find_eog_peaks == True:
-            eog_evoked = mne.preprocessing.create_eog_epochs(self.raw, picks='eeg', baseline=baseline, verbose=verbose).average()
+            eog_evoked = mne.preprocessing.create_eog_epochs(self.raw, picks='eeg', 
+            baseline=baseline, verbose=verbose).average()
             self.eog_evoked = eog_evoked
         if self.find_ecg_peaks == True:
-            ecg_evoked = mne.preprocessing.create_ecg_epochs(self.raw, picks='eeg', baseline=baseline, verbose=verbose).average()
+            ecg_evoked = mne.preprocessing.create_ecg_epochs(self.raw, picks='eeg', 
+            baseline=baseline, verbose=verbose).average()
             self.ecg_evoked = ecg_evoked
         return eog_evoked if 'eog_evoked' in locals() else None, ecg_evoked if 'ecg_evoked' in locals() else None
 
     def visualize_physsiological_evoked(self, evoked_epoch):
+
         evoked_epoch.plot_image(combine='mean')
         evoked_epoch.average().plot_joint()
         evoked_epoch.average().plot_image()
 
-    def find_physiological_artifacts(self, eog_treshold=0.8, ecg_treshold='auto', reject_by_annotation=True, measure='correlation', plot_fig=True, verbose='warning'):
+    def find_physiological_artifacts(self, eog_treshold=0.8, ecg_treshold='auto', 
+        reject_by_annotation=True, measure='correlation', plot_fig=True, verbose='warning'):
+
         if self.find_eog_peaks == True:
-            eog_indices, eog_scores = self.ica.find_bads_eog(self.raw, ch_name='EOG', threshold=eog_treshold, start=None, stop=None, l_freq=1, h_freq=10, reject_by_annotation=reject_by_annotation, measure=measure, verbose=verbose)
+            eog_indices, eog_scores = self.ica.find_bads_eog(self.raw, ch_name='EOG', 
+                threshold=eog_treshold, start=None, stop=None, l_freq=1, h_freq=10, 
+                reject_by_annotation=reject_by_annotation, measure=measure, verbose=verbose)
             if plot_fig == True:
                 # barplot of ICA component "EOG match" scores
                 self.ica.plot_scores(eog_scores)
@@ -127,13 +143,12 @@ class Indepndent_Component_Analysis:
                 # plot ICs applied to raw data, with EOG matches highlighted
                 self.ica.plot_sources(self.raw)
                 plt.show()
-                # plot ICs applied to the averaged EOG epochs, with EOG matches highlighted
-                self.ica.plot_sources(self.eog_evoked)
-                plt.show()
         else:
             print('Warning: EOG indices were not found.')
         if self.find_ecg_peaks == True:
-            ecg_indices, ecg_scores = self.ica.find_bads_ecg(self.raw, ch_name=None, threshold=ecg_treshold, start=None, stop=None, l_freq=8, h_freq=16, method='ctps', reject_by_annotation=True, measure=measure, verbose=verbose)
+            ecg_indices, ecg_scores = self.ica.find_bads_ecg(self.raw, ch_name=None, 
+            threshold=ecg_treshold, start=None, stop=None, l_freq=8, h_freq=16, method='ctps', 
+            reject_by_annotation=True, measure=measure, verbose=verbose)
             if plot_fig == True:
                 # barplot of ICA component "ECG match" scores
                 self.ica.plot_scores(ecg_scores)
@@ -142,17 +157,18 @@ class Indepndent_Component_Analysis:
                 # plot ICs applied to raw data, with ECG matches highlighted
                 self.ica.plot_sources(self.raw)
                 plt.show()
-                # plot ICs applied to the averaged EOG epochs, with EOG matches highlighted
-                self.ica.plot_sources(self.ecg_evoked)
-                plt.show()
         else:
             print('Warning: ECG indices were not found.')
         return eog_indices if 'eog_indices' in locals() else None, eog_scores if 'eog_scores' in locals() else None, ecg_indices if 'ecg_indices' in locals() else None, ecg_scores if 'ecg_scores' in locals() else None
-    
-'''
-eog_params
-ch_name=None, threshold=3.0, start=None, stop=None, l_freq=1, h_freq=10, reject_by_annotation=True, measure='zscore', verbose=None
 
-ecg_params
-ch_name=None, threshold='auto', start=None, stop=None, l_freq=8, h_freq=16, method='ctps', reject_by_annotation=True, measure='zscore', verbose=None
-'''
+    def perfrom_ICA(self):
+        eog_indices, ecg_indices = [], []
+        self.setup_ICA()
+        self.visualize_ICA_components()
+        eog_indices, _, ecg_indices, _ = self.find_physiological_artifacts(
+            eog_treshold=0.8, ecg_treshold='auto', reject_by_annotation=True,
+            measure='correlation', plot_fig=True, verbose='warning')
+        self.ica.exclude = self.exclude_ica(eog_indices + ecg_indices)
+        self.ica.apply(self.raw)
+        self.ica.plot_overlay(self.raw, exclude=self.ica.exclude, picks='eeg')
+        return self.raw
